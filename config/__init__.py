7fbddadfff29dc550c5992372379f97cb6e39174
kdwycz <kdwycz@gmail.com>
2016-05-02 11:09:13 +0800

    Chg: config

---
+++ config/__init__.py
@@ -0,0 +1,46 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+try:
+    from .config import *
+except ImportError:
+    from .config_sample import *
+
+
+class Config(object):
+    SECRET_KEY = FLASK['SECRET_KEY']
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = FLASK['SQLALCHEMY_COMMIT_ON_TEARDOWN']
+    JXNUGO_MAIL_SUBJECT_PREFIX = FLASK['JXNUGO_MAIL_SUBJECT_PREFIX']
+    JXNUGO_MAIL_SENDER = FLASK['JXNUGO_MAIL_SENDER']
+    JXNUGO_ADMIN = FLASK['JXNUGO_ADMIN']
+    SQLALCHEMY_DATABASE_URI = FLASK['SQLALCHEMY_DATABASE_URI']
+    JXNUGO_POSTS_PER_PAGE = JXNUGO['JXNUGO_POSTS_PER_PAGE']
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    MAIL_SERVER = MAIL['MAIL_SERVER']
+    MAIL_PORT = MAIL['MAIL_PORT']
+    MAIL_USE_TLS = MAIL['MAIL_USE_TLS']
+    MAIL_USERNAME = MAIL['MAIL_USERNAME']
+    MAIL_PASSWORD = MAIL['MAIL_PASSWORD']
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
+
+
+class ProductionConfig(Config):
+    TESTING = False
+
+
+configs = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig,
+}
--- manage.py
+++ manage.py
@@ -1,22 +1,18 @@
-#!usr/bin/env/ python
-#-*- coding: UTF-8 -*-
-from app import create_app,db
-import os
 from flask.ext.script import Manager,Shell
-from app.models import Role,User,Post
-from flask.ext.migrate import Migrate,MigrateCommand
-basedir=os.path.abspath(os.path.dirname(__file__))
+from app import create_app, db
+from app.models import Role, User, Post
+from flask.ext.migrate import Migrate, MigrateCommand
 
-app=create_app('default')
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@139.129.52.83:3306/jxnugo'
-app.config['SQLALCHEMY_DATABASE_URL'] = 'mysql://root:laidaolong@localhost:3306/jxnugo'
-manager=Manager(app)
-migrate=Migrate(app,db)
+app = create_app()
+manager = Manager(app)
+migrate = Migrate(app, db)
+
 
 def make_shell_context():
-    return dict(app=app,db=db,User=User,Role=Role,Post=Post)
-manager.add_command("shell",Shell(make_context=make_shell_context))
-manager.add_command('db',MigrateCommand)
+    return dict(app=app, db=db, User=User, Role=Role, Post=Post)
+manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     manager.run()
