48a73d4344239ae1c543865aa7a75845fcd4e6bf
Ddragonever <ddragonever@gmail.com>
2016-05-22 02:05:05 +0800

    fix rest-api register 401 bug

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -5,6 +5,7 @@ from ..models import Permission
 main=Blueprint('main',__name__)
 from . import errors,views
 
+
 @main.app_context_processor
 def inject_permissions():
     return dict(Permission=Permission)
