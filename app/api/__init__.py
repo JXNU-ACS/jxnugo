02a37ab35d448d410218e62283a3b656893baab2
Ddragonever <ddragonever@gmail.com>
2016-05-25 14:33:29 +0800

    add tour database

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -4,4 +4,4 @@ from flask import Blueprint
 
 api = Blueprint('api',__name__)
 
-from . import authentication, comments, errors, tradePosts, users, bbsposts , feedback
+from . import authentication, comments, errors, tradePosts, users, bbsposts, feedback, tour
