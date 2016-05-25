f06d058e99629287bf35149c3a5c2c8e6bf1e98a
can2 <279319529@qq.com>
2016-05-25 13:44:07 +0800

    补全必要的按钮，搜索，分类等等

--- app/trade/views.py
+++ app/trade/views.py
@@ -151,11 +151,9 @@ def query_post(problem):
         page, per_page=current_app.config['JXNUGO_POSTS_PER_PAGE'],
         error_out=False
     )
-    if pagination.total is not 0:
-        posts = pagination.items
-        return render_template('trade/trade_list.html', posts=posts, pagination=pagination)
-    else:
-        return render_template('404.html')
+    
+    posts = pagination.items
+    return render_template('trade/query_posts.html', posts=posts, pagination=pagination,problem=problem)
 
 
 @trade.route('/post_category/<posttag>')
